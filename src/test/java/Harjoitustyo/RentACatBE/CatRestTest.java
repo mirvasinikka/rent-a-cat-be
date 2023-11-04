366eb4fe7ec8c80ac545a689d27a19f1d5859730
mirvasinikka <mirva_s@hotmail.com>
2023-11-04 23:54:08 +0200

    add junit tests

--- src/test/java/Harjoitustyo/RentACatBE/CatRestTest.java
+++ src/test/java/Harjoitustyo/RentACatBE/CatRestTest.java
@@ -3,6 +3,7 @@ package Harjoitustyo.RentACatBE;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CatRestTest {
 	
 	@Autowired
---
+++ src/test/java/Harjoitustyo/RentACatBE/RentingRepositoryTest.java
@@ -0,0 +1,5 @@
+package Harjoitustyo.RentACatBE;
+
+public class RentingRepositoryTest {
+
+}
